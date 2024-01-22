fafe61f25f24ee0b7c132f357e6b194fbac6ae3f
bejit-i-pryachetsya <vaslol32@mail.ru>
2024-01-22 19:30:11 +0300

    Добавьте файлы проекта.

---
+++ Задание 2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Задание_2
+{
+    partial class Авторизация
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBox1.Location = new System.Drawing.Point(100, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(100, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.textBox2.Location = new System.Drawing.Point(100, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(161, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "  Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(161, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(77, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Авторизация в системе";
+            // 
+            // Авторизация
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(385, 323);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Авторизация";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Задание 2/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using Npgsql.Internal;
+
+namespace Задание_2
+{
+    public partial class Авторизация : Form
+    {
+        private System.Windows.Forms.Timer unlockTimer;
+        public bool DB(string x, string c)
+        {
+
+            NpgsqlConnection conn = new NpgsqlConnection("User ID = postgres; Password = 12345; Host = localhost; Port = 5432;  Database = postgres;");
+            conn.Open();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            string sql = ($"SELECT * FROM polz WHERE polzlogin = '{x}' and polzpassword = '{c}';");
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+            ds.Reset();
+            da.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+            
+            conn.Close();
+
+
+
+
+        }
+            
+
+        public Авторизация()
+        {
+            InitializeComponent();
+            unlockTimer = new System.Windows.Forms.Timer();
+            unlockTimer.Tick += unlockTimer_Tick;
+        }
+
+       
+        private int invalidAttempts = 0;
+        private DateTime lockoutEndTime;
+
+        private void unlockTimer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= lockoutEndTime)
+            {
+                // Тайм-аут окончен
+                invalidAttempts = 0;
+                button1.Enabled = true;
+                label3.Text = "Авторизация в системе";
+                unlockTimer.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == default ^ textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Введите логин и пароль", "Ошибка!");
+
+            }
+            else if (textBox2.Text == default ^ textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Введите логин и пароль", "Ошибка!");
+
+            }
+            else
+            {
+                bool x = DB(textBox1.Text, textBox2.Text);
+                if (x == true)
+                {
+                    MessageBox.Show("Вы успешно авторизировались!", "Ураа!");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+                else if (x == false)
+                {
+                    MessageBox.Show("Неверный логин или пароль","Ошибка!");
+                    invalidAttempts++;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    if (invalidAttempts >= 3)
+                    {
+                        lockoutEndTime = DateTime.Now.AddSeconds(20);
+                        button1.Enabled = false;
+
+                        // Запуск таймера для разблокировки через 20 секунд
+                        label3.Text = "Ошибка! Попробуйте позже.";
+                        
+                        unlockTimer.Start();
+                    }
+                }
+            }
+                   
+            
+            
+            
+        }
+    }
+}
